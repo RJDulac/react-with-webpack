8471f1094c333e8742247ef758374b898aaa76f7
Ryan Dulac <analyticrye@gmail.com>
2018-06-19 21:10:40 -0400

    webpack installed

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+
+	entry: path.join(__dirname, 'src', 'app.js'),
+
+	output: {
+
+		path: path.join(__dirname, 'dist'),
+
+		filename: 'app.bundle.js'
+	},
+
+	mode: process.env.NODE_ENV || 'development'
+
+};
