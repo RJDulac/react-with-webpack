2e3fee98820b548f77049e5892d4b9a70b219168
Ryan Dulac <analyticrye@gmail.com>
2018-06-19 22:56:53 -0400

    server added

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
 
 	entry: path.join(__dirname, 'src', 'app.js'),
@@ -21,6 +23,17 @@ module.exports = {
 
 			use: ['babel-loader']
 		}]
-	}
+	},
+	devServer: {
+		contentBase: path.join(__dirname, 'src')
+	},
+
+	plugins: [
+
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, 'src', 'index.html')
+		})
+
+	]
 
 };
